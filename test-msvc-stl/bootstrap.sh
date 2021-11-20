ec6bda90d047b8211860b70fe87663d8b654ccbe
Michael Kazakov <mike.kazakov@gmail.com>
2021-11-20 12:15:05 +0000

    adding tests from MSVC STL

---
+++ test-msvc-stl/bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+git clone \
+  --depth 1  \
+  --filter=blob:none  \
+  --no-checkout \
+  https://github.com/microsoft/STL.git
+
+cd STL
+git sparse-checkout init --cone
+git sparse-checkout add tests
+git checkout
